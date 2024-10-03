b010418b5896d82b1c5a1184e7165af955222fe3
Rayssa <158166558+raygoncalvesss@users.noreply.github.com>
2024-10-03 08:53:04 -0300

    feat: create index routes file

---
+++ src/routes/index.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import personagensRouter from "./personagens.routes.js";
+import emocoesRoutes from "./emocoes.routes.js";
+
+const routes = Router();
+
+routes.get("/", (req, res) => {
+    return res.status(200).send({message:"spfc"})
+});
+
+routes.get("/2tds2", (req,res) => {
+    return res.status(200).send({message:"hello 2tds2"})
+});
+
+routes.use("/emocoes", emocoesRoutes);
+routes.use("/personagens", personagensRouter);
+
+export default routes;
+
